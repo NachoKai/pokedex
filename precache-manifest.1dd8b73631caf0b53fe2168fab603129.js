3e2b8f6ecbce006dede317df4e3adc6adb2b39e5
NachoKai <nachokai@outlook.com>
2020-05-01 14:47:43 -0300

    Updates

---
+++ precache-manifest.1dd8b73631caf0b53fe2168fab603129.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5510b3eaad87706e376a1aec420e1e8",
+    "url": "/pokedex/index.html"
+  },
+  {
+    "revision": "742ac8cc830f8bab288e",
+    "url": "/pokedex/static/css/main.e0c0b704.chunk.css"
+  },
+  {
+    "revision": "b518d1f169265bc6e42e",
+    "url": "/pokedex/static/js/2.a37dfa6d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pokedex/static/js/2.a37dfa6d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "742ac8cc830f8bab288e",
+    "url": "/pokedex/static/js/main.e606b050.chunk.js"
+  },
+  {
+    "revision": "31e1896eb3ee7d0f11ce",
+    "url": "/pokedex/static/js/runtime-main.cd23c455.js"
+  }
+]);
