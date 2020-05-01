3e792414ed647be4f88154eca1920df0528a2148
NachoKai <nachokai@outlook.com>
2020-05-01 20:29:52 -0300

    Commit

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
   return (
     <Router>
       <Switch>
-        <Route path="pokedex/modal">
+        <Route path="/modal">
           <Modal />
         </Route>
         <Route path="/">
