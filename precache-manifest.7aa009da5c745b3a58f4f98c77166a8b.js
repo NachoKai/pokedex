a347d32a4b552113e88415c7d5d42db203cb455e
Nacho Caiafa <nachokai@outlook.com>
2020-08-30 01:27:52 -0300

    Updates

--- precache-manifest.7aa009da5c745b3a58f4f98c77166a8b.js
+++ precache-manifest.7aa009da5c745b3a58f4f98c77166a8b.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "b589c73adb0d868e63ca0ef52aa4aa89",
+    "revision": "0e230d4eee18eb17aed0a8945273d3bf",
     "url": "/pokedex/index.html"
   },
   {
-    "revision": "8b9f1d520483981321d5",
+    "revision": "1e4efec99040645f17bb",
     "url": "/pokedex/static/css/main.1acf25af.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/pokedex/static/js/2.e106d6fc.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "8b9f1d520483981321d5",
-    "url": "/pokedex/static/js/main.f43a06c6.chunk.js"
+    "revision": "1e4efec99040645f17bb",
+    "url": "/pokedex/static/js/main.69adf71b.chunk.js"
   },
   {
     "revision": "0125f0f48fe08fd772f3",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/pokedex/precache-manifest.437e32476a8eb892c825c990d58d7f1b.js"
+  "/pokedex/precache-manifest.7aa009da5c745b3a58f4f98c77166a8b.js"
 );
 
 self.addEventListener('message', (event) => {
